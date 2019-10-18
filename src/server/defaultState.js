a49766588bbd7a501807e37f0d2e59d87bbfd034
Jessica McKenzie <contact@jessmckenzie.com>
2019-10-18 09:50:13 +0100

    add dashboard and tasklist views

---
+++ src/server/defaultState.js
@@ -0,0 +1,53 @@
+export const defaultState = {
+  users: [
+    {
+      id: "U1",
+      name: "Jess"
+    },
+    {
+      id: "U2",
+      name: "Ness"
+    }
+  ],
+  groups: [
+    {
+      name: "To Do",
+      id: "G1",
+      owner: "U1"
+    },
+    {
+      name: "Doing",
+      id: "G2",
+      owner: "U1"
+    },
+    {
+      name: "Done",
+      id: "G3",
+      owner: "U1"
+    }
+  ],
+  tasks: [
+    {
+      name: "Do tests",
+      id: "T1",
+      group: "G1",
+      owner: "U1",
+      isComplete: false
+    },
+    {
+      name: "Something else",
+      id: "T2",
+      group: "G2",
+      owner: "U1",
+      isComplete: false
+    }
+  ],
+  comments: [
+    {
+      owner: "U1",
+      id: "C1",
+      task: "T1",
+      content: "Great work!"
+    }
+  ]
+};
